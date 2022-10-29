994321e47992314fe9969f612448b683cd417c1f
Federico Castelli <fc@oracleofde.fi>
2022-10-29 16:12:01 +0100

    Trying to use web3modal transactions

--- webapp/src/hooks/useCreateCgProject.ts
+++ webapp/src/hooks/useCreateCgProject.ts
@@ -2,9 +2,12 @@ import Web3 from "web3";
 import {useState} from "react";
 import {useAppDispatch} from "./reduxHooks";
 import {AbiItem} from "web3-utils";
+import {Provider} from "@web3modal/ethereum";
+import {ContractInterface, ethers} from "ethers";
+import {useContract, useSendTransaction} from "@web3modal/react";
 
 export interface CreateCgProjectInterface {
-  web3: Web3,
+  web3: ethers.providers.Provider,
   fromAddress: string,
   cgFactoryAbi: AbiItem,
   cgFactoryAddress: string,
@@ -15,14 +18,30 @@ export interface CreateCgProjectInterface {
 
 export const useCreateCgProject = (params: CreateCgProjectInterface) => {
   const [status, setStatus] = useState<{
-    loading: boolean,
+    transactionHash: string,
     error: string,
     tokenAddress: string
-  }>({loading: false, error: "", tokenAddress: ""});
+  }>({transactionHash: "", error: "", tokenAddress: ""});
   const dispatch = useAppDispatch();
+  const { contract, isReady } = useContract({
+    address: params.cgFactoryAddress,
+    abi: [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"uint256","name":"_githubID","type":"uint256"},{"internalType":"address","name":"_wallet","type":"address"}],"name":"addAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressToGithubID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"githubIDToAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_githubID","type":"uint256"},{"internalType":"address","name":"_addressToRemove","type":"address"}],"name":"removeAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+  });
   const checkNow = (params: CreateCgProjectInterface) => {
-    setStatus({loading: true, error: "", tokenAddress: ""});
+    setStatus({transactionHash: "", error: "", tokenAddress: ""});
     // TODO: understand if this part of code is working
+
+    contract.methods.generate(params.tokenName, params.tokenSymbol, params.prevContrRewards).then(result => {
+      console.log(result);
+      return result.wait()
+          .then(rc => {
+            const event = rc?.events?.find(event => event.event === 'NewCgTokenCreated');
+            console.log(event);
+            // const [_addr, _name, _symbol, _percFundingDistribute] = event?.args as NewCgTokenCreatedEvent;
+          })
+    });
+
+    /*
     let cgTokenContract = new params.web3.eth.Contract(params.cgFactoryAbi, params.cgFactoryAddress);
     cgTokenContract.methods.generate(params.tokenName, params.tokenSymbol, params.prevContrRewards).estimateGas({
       value: 0,  // TODO: understand what I have to put here as value
@@ -37,9 +56,9 @@ export const useCreateCgProject = (params: CreateCgProjectInterface) => {
           gas: Math.floor(parseInt(estimatedGas) * 1.1)
         })
             .on('transactionHash', (hash: string) => {
-              console.log(hash);
+              setStatus({transactionHash: hash, error: "", tokenAddress: ""});
             })
-            .on('receipt', function(receipt: string){
+            .on('receipt', (receipt) => {
               console.log(receipt);
             })
             .on('error', (e: any) => {
@@ -47,6 +66,7 @@ export const useCreateCgProject = (params: CreateCgProjectInterface) => {
             });
       }
     });
+     */
   }
   return {
     ...status, checkNow
--- webapp/src/ui/pages/Home/Home.tsx
+++ webapp/src/ui/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Box, Button} from "@mui/material";
 import {useAccount, useConnectModal, useProvider} from "@web3modal/react";
+import {useCreateCgProject} from "../../../hooks/useCreateCgProject";
+import {CONTRACTS_DETAILS} from "../../../utils/constants";
 
 /**
  *
@@ -13,6 +15,16 @@ const Home: React.FC<IHome> = (props) => {
   const { account } = useAccount();
   const { provider } = useProvider();
 
+  const {tokenAddress, transactionHash, error, checkNow} = useCreateCgProject({
+    tokenName: "First Test",
+    tokenSymbol: "TEST",
+    prevContrRewards: 50,
+    web3: provider,
+    cgFactoryAddress: CONTRACTS_DETAILS[1337].CG_FACTORY,
+    cgFactoryAbi: CONTRACTS_DETAILS[1337].CG_FACTORY_ABI,
+    fromAddress: account.address
+  });
+
   const test = () => {
 
   }
@@ -22,6 +34,17 @@ const Home: React.FC<IHome> = (props) => {
   return (
     <Box display={"flex"} alignItems={"center"} justifyContent={"center"} height="100vh">
       <Button onClick={open} variant="contained">
+        ciao
+      </Button>
+      <Button onClick={() => {checkNow({
+        tokenName: "First Test",
+        tokenSymbol: "TEST",
+        prevContrRewards: 50,
+        web3: provider,
+        cgFactoryAddress: CONTRACTS_DETAILS[1337].CG_FACTORY,
+        cgFactoryAbi: CONTRACTS_DETAILS[1337].CG_FACTORY_ABI,
+        fromAddress: account.address
+      })}} variant="contained">
         ciao
       </Button>
     </Box>

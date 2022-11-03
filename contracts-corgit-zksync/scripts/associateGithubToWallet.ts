d10cac10ab33d199dc11584012588692982a617d
Antonio <antonio.giovanni.seveso@gmail.com>
2022-11-03 20:07:53 +0100

    - added script to deploy github addresses

---
+++ contracts-corgit-zksync/scripts/associateGithubToWallet.ts
@@ -0,0 +1,46 @@
+import {Contract, Provider, Wallet} from "zksync-web3";
+import secrets from "../.secrets.json";
+import githubAddressRegister from "../artifacts-zk/contracts/GithubAddressRegister.sol/GithubAddressRegister.json";
+
+const GITHUB_REGISTER_ADDRESS = "0xa5B07286eA9a9f7deC44104Cb621f1cf55AA9634";
+
+
+export const associateGithubToWallet = async (
+  githubAddressRegisterContract: string,
+  githubId: number,
+  walletAddress: string
+): Promise<{
+
+}> => {
+  const provider = new Provider('https://zksync2-testnet.zksync.dev');
+  const signer = new Wallet(secrets.privateKeys.zksync.deployer, provider);
+
+  const githubContract = new Contract(
+    githubAddressRegisterContract,
+    githubAddressRegister.abi,
+    signer
+  );
+
+  const transaction = await githubContract.connect(signer).addAddress(githubId, walletAddress);
+  console.log(transaction);
+
+  return {};
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+  associateGithubToWallet(
+    GITHUB_REGISTER_ADDRESS,
+      // 31770652,
+      // "0x349F4A96a44fcd83338b90DC37Fb7F5FeEc8AdE1"
+    12898752,
+      "0x6cA960968E33F9350a3B4522a673f5d8438c9aAf"
+  )
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+
+
